251e36daf352b7449a306fe7ad410125031ffa4f
Robert Beekman <robert@matsimitsu.nl>
2016-08-05 08:20:52 +0200

    Track consumed metrics with statsD

--- README.md
+++ README.md
@@ -16,5 +16,42 @@ downloads dependencies and runs builds and tests.
 
 ```
 brew install gradle
+```
 
 Then run the tests: `gradle test`.
+And build: `gradle build`.
+
+## Enable the interceptor
+
+1. Make sure the build `.jar` is in your classpath
+2. Add the interceptor(s) to your properties:
+
+```
+producer.interceptor.classes=com.appsignal.kafka.StatsdProducerInterceptor
+consumer.interceptor.classes=com.appsignal.kafka.StatsdConsumerInterceptor
+```
+
+## Configure the interceptor
+
+Accepted configuration:
+
+```
+statsd.host   (defaults to "localhost")
+statsd.port   (defaults to 8125)
+statsd.prefix (defaults to "")
+```
+
+For either the `consumer` or the `producer` interceptor set the config:
+
+```
+# Producer
+producer.interceptor.statsd.host=localhost
+producer.interceptor.config.statsd.host=8125
+producer.interceptor.statsd.prefix=kafka
+
+# Consumer
+consumer.interceptor.statsd.host=localhost
+consumer.interceptor.config.statsd.host=8125
+consumer.interceptor.statsd.prefix=kafka
+
+```
--- src/main/kotlin/com/appsignal/kafka/statsd_consumer_interceptor.kt
+++ src/main/kotlin/com/appsignal/kafka/statsd_consumer_interceptor.kt
@@ -1,6 +1,6 @@
 package com.appsignal.kafka
 
-import java.lang.System.*
+import java.net.InetAddress
 
 import com.timgroup.statsd.StatsDClient
 import com.timgroup.statsd.NonBlockingStatsDClient
@@ -8,35 +8,76 @@ import com.timgroup.statsd.NonBlockingStatsDClient
 import org.apache.kafka.clients.consumer.ConsumerInterceptor
 import org.apache.kafka.common.Configurable
 import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.config.ConfigDef
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.ConsumerRecord
 
+import org.slf4j.LoggerFactory
+
 class StatsdConsumerInterceptor : ConsumerInterceptor<Any, Any> {
+  var statsd: StatsDClient? = null
+  var hostname: String? = null
 
   override fun configure(configs: Map<String,*>) {
-    out.println("Configs: ${configs}")
+    log.info("Configs: ${configs}")
+    var ip = InetAddress.getLocalHost();
+    hostname = ip.getHostName();
+
+    var host = (configs.get(STATSD_HOST) ?: STATSD_HOST_DEFAULT) as String
+    var port = (configs.get(STATSD_PORT) ?: STATSD_PORT_DEFAULT) as Int
+    var prefix = (configs.get(STATSD_PREFIX) ?: STATSD_PREFIX_DEFAULT) as String
+
+    log.info("Starting StatsD ConsumerInterceptor with config: ${host}:${port}/${prefix}")
+    statsd = NonBlockingStatsDClient(prefix, host, port)
   }
 
   override fun onConsume(records: ConsumerRecords<Any, Any>):ConsumerRecords<Any, Any> {
+    // Send a globla count for this host
+    statsd!!.count("kafka.consumer.host.${hostname!!}.messages", records.count().toLong())
 
-    out.println("Record count: ${records.count()}")
-    out.println("Record partitions: ${records.partitions()}")
+    // Send a count and lag for each partition/topic
+    for (partition in records.partitions()) {
+      var count = records.records(partition).count().toLong()
 
-    for (record in records) {
+      statsd!!.count("kafka.consumer.topic.${partition.topic()}.messages", count)
+      statsd!!.count("kafka.consumer.topic.${partition.topic()}.partition.${partition.partition()}.messages", count)
 
-      out.println("Record: ${record}")
+      // Calculate the lag time, only use the first record sice it's the oldest in the queue?
+      if (!records.isEmpty()) {
+        var lag = System.currentTimeMillis() - records.first().timestamp()
+
+        statsd!!.recordExecutionTime("kafka.consumer.topic.${partition.topic()}.lag", lag)
+        statsd!!.recordExecutionTime("kafka.consumer.topic.${partition.topic()}.partition.${partition.partition()}.lag", lag)
+      }
     }
 
     return records;
   }
 
   override fun onCommit(offsets: Map<TopicPartition, OffsetAndMetadata>) {
-
-    out.println("Records: ${offsets}")
+    // Nothing to do
   }
 
   override fun close() {
     // Nothing to do
   }
+
+  companion object {
+    val STATSD_HOST = "interceptor.statsd.host"
+    val STATSD_PORT = "interceptor.statsd.port"
+    val STATSD_PREFIX = "interceptor.statsd.prefix"
+
+    val STATSD_HOST_DEFAULT = "localhost"
+    val STATSD_PORT_DEFAULT = 8125
+    val STATSD_PREFIX_DEFAULT = ""
+
+    private val CONFIG_DEF = ConfigDef()
+      .define(STATSD_HOST, ConfigDef.Type.STRING, STATSD_HOST_DEFAULT, ConfigDef.Importance.HIGH, "StatsD host")
+      .define(STATSD_PORT, ConfigDef.Type.INT, STATSD_PORT_DEFAULT, ConfigDef.Importance.HIGH, "StatsD port")
+      .define(STATSD_PREFIX, ConfigDef.Type.STRING, STATSD_PREFIX_DEFAULT, ConfigDef.Importance.LOW, "StatsD prefix")
+
+    private val log = LoggerFactory.getLogger(ConsumerInterceptor::class.java)
+  }
+
 }
